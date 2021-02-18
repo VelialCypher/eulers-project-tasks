f1697d625c7d5f391491389afd5a8037f21510e6
VelialCypherÐ <stug_kt@mail.ru>
2021-02-19 00:23:27 +0300

    swap task7 <-> task8

--- task7.c
+++ task7.c
@@ -2,79 +2,34 @@
 #include <stdio.h>
 
 /*
-The four adjacent digits in the 1000-digit
-	number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+	that the 6th prime is 13.
 
-	73167176531330624919225119674426574742355349194934
-	96983520312774506326239578318016984801869478851843
-	85861560789112949495459501737958331952853208805511
-	12540698747158523863050715693290963295227443043557
-	66896648950445244523161731856403098711121722383113
-	62229893423380308135336276614282806444486645238749
-	30358907296290491560440772390713810515859307960866
-	70172427121883998797908792274921901699720888093776
-	65727333001053367881220235421809751254540594752243
-	52584907711670556013604839586446706324415722155397
-	53697817977846174064955149290862569321978468622482
-	83972241375657056057490261407972968652414535100474
-	82166370484403199890008895243450658541227588666881
-	16427171479924442928230863465674813919123162824586
-	17866458359124566529476545682848912883142607690042
-	24219022671055626321111109370544217506941658960408
-	07198403850962455444362981230987879927244284909188
-	84580156166097919133875499200524063689912560717606
-	05886116467109405077541002256983155200055935729725
-	71636269561882670428252483600823257530420752963450
-
-Find the thirteen adjacent digits in the 1000-digit number that have 
-	the greatest product. What is the value of this product?
+What is the 10 001st prime number?
 */
 
-#define BUFFER_SIZE 1000
-
-void readFile(char* buffer, char* fileName) {
-	FILE *file;
-	char i = 0;
-	int x = 0;
-
-	file = fopen(fileName, "r");
-
-	while ((i = fgetc(file)) != EOF) {
-		if(i != '\n') buffer[x++] = i;
-	}
-
-	fclose(file);
-}
-
-int test(int count, char* data) {
-	int position = 0;
-	int x = 0;
-	int mul = 1;
-	int next = 1;
-
-	while (position <= BUFFER_SIZE - count) {
-		if(x < count) {
-			next *= (data[position + x] - '0');
-			x++;
-		} else {
-			if(next > mul) mul = next;
-			next = 1;
-			position++;
-			x = 0;
+int isPrime(int x) {
+	for (size_t i = 2; i < x; i++) {
+		if(x % i == 0) {
+			return 0;
 		}
 	}
+	return 1;
+}
 
-	return mul;
+int test(int bellow) {
+	int i = 0;
+	int y = 1;
+
+	while (i != bellow) {
+		if(isPrime(++y)) i++;
+	}
+	
+	return y;
 }
 
 int main(int argc, char* argv[]) {
-	char str[BUFFER_SIZE];
-	readFile(&str, "dataTask7.txt");
-
-	// 5832
-	printf("the four adjacent digits in the 1000-digit number: %d\n", test(4, &str));
-
-	// 2091059712 - todo
-	printf("the thirteen adjacent digits in the 1000-digit number: %d\n", test(13, &str));
+	printf("is the 6st prime number: %d\n", test(6)); // 13
+	printf("is the 10 001st prime number: %d\n", test(10001)); // 104743
 	return 0;
 }
